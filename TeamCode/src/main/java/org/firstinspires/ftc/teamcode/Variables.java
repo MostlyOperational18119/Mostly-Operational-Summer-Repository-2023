d2dd0201bb2eb890ee6dbad7fe288882f626d59e
DamienB325 <damien.brown15@outlook.com>
2023-03-17 16:48:28 -0700

    Add Basic Odometry.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Variables.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.arcrobotics.ftclib.geometry.Translation2d;
+import com.arcrobotics.ftclib.kinematics.wpilibkinematics.MecanumDriveKinematics;
+import com.arcrobotics.ftclib.kinematics.wpilibkinematics.MecanumDriveOdometry;
+
+public class Variables {
+    static Translation2d m_frontLeftLocation = null;
+    static Translation2d m_frontRightLocation = null;
+    static Translation2d m_backLeftLocation = null;
+    static Translation2d m_backRightLocation = null;
+    static MecanumDriveKinematics m_kinematics = null;
+    static MecanumDriveOdometry m_odometry = null;
+}
