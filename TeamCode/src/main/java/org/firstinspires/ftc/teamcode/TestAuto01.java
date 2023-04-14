622d03cae967f0ab8174882786161c4e026f2b4b
DamienB325 <damien.brown15@outlook.com>
2023-04-14 16:25:19 -0700

    Migrate to the vastly superior Roadrunner Odometry library.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuto01.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestAuto01.java
@@ -29,6 +29,7 @@
 
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -50,7 +51,7 @@ import com.qualcomm.robotcore.util.ElapsedTime;
  */
 
 
-
+@Disabled
 @TeleOp(name="TestAuto01", group="Linear Opmode")
 public class TestAuto01 extends LinearOpMode {
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDrive01.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestDrive01.java
@@ -51,7 +51,7 @@ import com.qualcomm.robotcore.util.Range;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
-
+@Disabled
 @TeleOp(name="TestDrive01", group="Linear Opmode")
 public class TestDrive01 extends LinearOpMode {
 
