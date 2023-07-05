6f99bca0b485d32afd3418c2d8397e46682b4e34
BetheBob1 <BetheBob082208@gmail.com>
2023-07-05 11:53:24 -0700

    Update StandardTrackingWheelLocalizer.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/drive/StandardTrackingWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunner/drive/StandardTrackingWheelLocalizer.java
@@ -31,7 +31,7 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
     public static double WHEEL_RADIUS = .688975; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (encoder) speed
 
-    public static double LATERAL_DISTANCE = 16.18; // in; distance between the left and right wheels
+    public static double LATERAL_DISTANCE = 16.15; // in; distance between the left and right wheels
     public static double FORWARD_OFFSET = 8; // in; offset of the lateral wheel
 
     public static double X_MULTIPLIER = 1.03358030027;
@@ -55,6 +55,7 @@ public class StandardTrackingWheelLocalizer extends ThreeTrackingWheelLocalizer 
         frontEncoder = new Encoder(hardwareMap.get(DcMotorEx.class, "FR"));
 
         // TODO: reverse any encoders using Encoder.setDirection(Encoder.Direction.REVERSE)
+        frontEncoder.setDirection(Encoder.Direction.REVERSE);
     }
 
     public static double encoderTicksToInches(double ticks) {
